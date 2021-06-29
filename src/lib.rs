3c2c8bc00b28557e63c6d9dfcd029627e2cf197d
bOli <github@bueechi.net>
2021-06-29 11:49:29 +0200

    Weird thing that solves the example but fails miserably on part1, because it's totally wrong ;)

---
+++ src/lib.rs
@@ -0,0 +1,138 @@
+use std::collections::{HashMap, HashSet};
+
+mod tests;
+
+#[derive(Debug, PartialEq)]
+struct Validator {
+    valid_messages: HashSet<String>,
+}
+impl Validator {
+    fn is_valid(&self, message: &str) -> bool {
+        self.valid_messages.contains(message)
+    }
+}
+
+#[derive(Debug)]
+struct Resolver {
+    resolved_rules: HashMap<usize, Vec<String>>,
+    unresolved_rules: HashMap<usize, Vec<Vec<usize>>>,
+}
+impl From<&[String]> for Resolver {
+    fn from(rules: &[String]) -> Resolver {
+        let mut resolved: HashMap<usize, Vec<String>> = HashMap::new();
+        let mut unresolved: HashMap<usize, Vec<Vec<usize>>> = HashMap::new();
+        for rule in rules {
+            if let Some((left, right)) = rule.split_once(": ") {
+                let index: usize = left.parse().unwrap();
+                if right.starts_with('\"') && right.ends_with('\"') {
+                    let char = right.chars().nth(1).unwrap();
+                    resolved.insert(index, vec![char.to_string()]);
+                } else {
+                    let sequences: Vec<Vec<usize>> = right
+                        .split(" | ")
+                        .map(|seq| {
+                            seq.split_ascii_whitespace()
+                                .filter_map(|c| c.parse().ok())
+                                .collect()
+                        })
+                        .collect();
+                    unresolved.insert(index, sequences);
+                }
+            } else {
+                panic!("Invalid input rule '{}'", rule)
+            };
+        }
+        Resolver {
+            resolved_rules: resolved,
+            unresolved_rules: unresolved,
+        }
+    }
+}
+impl Resolver {
+    fn is_resolvable(resolved: &HashMap<usize, Vec<String>>, sequences: &[Vec<usize>]) -> bool {
+        sequences
+            .iter()
+            .flat_map(|indices| indices.iter())
+            .all(|idx| resolved.contains_key(idx))
+    }
+    fn resolve(mut self) -> Validator {
+        println!("{:?}", self);
+        while !self.unresolved_rules.is_empty() {
+            // Moved to avoid borrow-checker error when accessing self.resolveds in the loop
+            let mut to_resolve = self.unresolved_rules;
+            self.unresolved_rules = HashMap::new();
+            for (index, sequences) in to_resolve.drain() {
+                println!("Trying to resolve ({}, {:?})", index, sequences);
+                if Resolver::is_resolvable(&self.resolved_rules, &sequences) {
+                    let mut outer_strings: Vec<String> = vec![];
+                    for indices in &sequences {
+                        let resolved: Vec<&Vec<String>> = indices
+                            .iter()
+                            .map(|i| self.resolved_rules.get(i).unwrap())
+                            .collect();
+                        println!("Resolved = {:?}", resolved);
+                        let inner_strings = Resolver::multiply(&resolved);
+                        println!("Resolved inner {:?} to {:?}", indices, inner_strings);
+                        outer_strings.extend(inner_strings);
+                    }
+                    println!("Resolved outer {:?} to {:?}", sequences, outer_strings);
+                    self.resolved_rules.insert(index, outer_strings);
+                } else {
+                    // cannot fully resolve this rule yet
+                    println!("Skipping and re-adding ({}, {:?})", index, sequences);
+                    self.unresolved_rules.insert(index, sequences);
+                }
+            }
+            println!("{:?}", self);
+        }
+
+        let rule_0 = self.resolved_rules.get(&0).unwrap();
+        // println!("rule_0 = {:?}", rule_0);
+        let valid_messages = rule_0.iter().cloned().collect();
+        Validator { valid_messages }
+    }
+    fn multiply(sequences: &[&Vec<String>]) -> Vec<String> {
+        let first = sequences[0];
+        if sequences.len() == 1 {
+            vec![first.join("")]
+        } else if sequences.len() == 2 {
+            let second = sequences[1];
+            // println!("a = {:?}", first);
+            // println!("b = {:?}", second);
+            let result = first
+                .iter()
+                .map(|a| {
+                    second
+                        .clone()
+                        .iter()
+                        .map(|b| a.clone() + b)
+                        .collect::<Vec<_>>()
+                })
+                .flatten()
+                .collect();
+            // println!("c = {:?}\n-----", result);
+            result
+        } else {
+            let second = Resolver::multiply(&sequences[1..]);
+            Resolver::multiply(&[first, &second])
+        }
+    }
+    fn join_sequences(sequences: &[&Vec<String>]) -> Vec<String> {
+        sequences.iter().map(|sequence| sequence.join("")).collect()
+    }
+}
+
+fn number_of_messages_matching_rule_0(input: &[String]) -> usize {
+    let mut split = input.split(|line| line.is_empty());
+    let (rules, messages) = (split.next().unwrap(), split.next().unwrap());
+
+    // println!("Rules: {:?}", rules);
+    // println!("Messages: {:?}", messages);
+
+    let validator = Resolver::from(rules).resolve();
+    // println!("Valid messages: {:?}", validator.valid_messages);
+    messages
+        .iter()
+        .filter(|message| validator.is_valid(message))
+        .count()
+}
---
+++ src/tests.rs
@@ -0,0 +1,146 @@
+#[cfg(test)]
+use crate::*;
+use line_reader::{read_file_to_lines, read_str_to_lines};
+
+const EXAMPLE: &str = "0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: \"a\"
+5: \"b\"
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb";
+
+#[test]
+fn test_join_sequences_with_example_rule4() {
+    // rule 4: "a"
+    assert_eq!(Resolver::multiply(&[&vec!["a".to_string()]]), vec!["a"]);
+}
+
+#[test]
+fn test_join_sequences_with_simple_rule() {
+    assert_eq!(
+        Resolver::join_sequences(&[&vec!["a".to_string()], &vec!["b".to_string()]]),
+        vec!["a", "b"]
+    );
+}
+
+#[test]
+fn test_join_sequences_with_example_rule2() {
+    assert_eq!(
+        // rule 2: 4 4 | 5 5
+        Resolver::join_sequences(&[
+            &vec!["a".to_string(), "a".to_string()],
+            &vec!["b".to_string(), "b".to_string()],
+        ]),
+        vec!["aa", "bb",]
+    );
+}
+
+#[test]
+fn test_join_sequences_with_example_rule3() {
+    assert_eq!(
+        // rule 3: 4 5 | 5 4
+        Resolver::join_sequences(&[
+            &vec!["a".to_string(), "b".to_string()],
+            &vec!["b".to_string(), "a".to_string()]
+        ]),
+        vec!["ab", "ba"]
+    );
+}
+
+#[test]
+fn test_join_sequences_with_example_rule1() {
+    assert_eq!(
+        // rule 1: 2 3 | 3 2
+        Resolver::join_sequences(&[
+            // 2 3:
+            &vec!["aa".to_string(), "ab".to_string(),],
+            &vec!["aa".to_string(), "ba".to_string(),],
+            &vec!["bb".to_string(), "ab".to_string(),],
+            &vec!["bb".to_string(), "ba".to_string(),],
+            // 3 2:
+            &vec!["ab".to_string(), "aa".to_string(),],
+            &vec!["ab".to_string(), "bb".to_string(),],
+            &vec!["ba".to_string(), "aa".to_string(),],
+            &vec!["ba".to_string(), "bb".to_string(),],
+        ]),
+        vec!["aaab", "aaba", "bbab", "bbba", "abaa", "abbb", "baaa", "babb"]
+    );
+}
+
+#[test]
+fn test_multiply_with_example_rules() {
+    assert_eq!(
+        Resolver::multiply(&[
+            &vec!["aa".to_string(), "bb".to_string()],
+            &vec!["ab".to_string(), "ba".to_string()],
+        ]),
+        vec!["aaab", "aaba", "bbab", "bbba"]
+    );
+}
+
+#[test]
+fn test_multiply_more() {
+    assert_eq!(
+        Resolver::multiply(&[
+            &vec!["a".to_string(), "b".to_string()],
+            &vec!["c".to_string()]
+        ]),
+        vec!["ac", "bc"]
+    );
+    assert_eq!(
+        Resolver::multiply(&[
+            &vec!["a".to_string(), "b".to_string()],
+            &vec!["c".to_string(), "d".to_string()]
+        ]),
+        vec!["ac", "ad", "bc", "bd"]
+    );
+    assert_eq!(
+        Resolver::multiply(&[
+            &vec!["a".to_string(), "b".to_string()],
+            &vec!["c".to_string(), "d".to_string()],
+            &vec!["e".to_string(), "f".to_string()]
+        ]),
+        vec!["ace", "acf", "ade", "adf", "bce", "bcf", "bde", "bdf"]
+    );
+}
+
+#[test]
+fn example1() {
+    assert_eq!(
+        number_of_messages_matching_rule_0(&read_str_to_lines(EXAMPLE)),
+        2
+    );
+}
+
+// #[test]
+// fn another_example() {
+//     let rules = read_str_to_lines(
+//         "0: 1 2 | 3 4 | 5 6
+// 1: \"a\"
+// 2: \"b\"
+// 3: \"c\"
+// 4: \"d\"
+// 5: \"e\"
+// 6: \"f\"",
+//     );
+//     assert_eq!(
+//         Resolver::from(rules.as_slice()).resolve(),
+//         Validator {
+//             valid_messages: HashSet::new()
+//         }
+//     );
+// }
+
+// #[test]
+fn part1() {
+    assert_eq!(
+        number_of_messages_matching_rule_0(&read_file_to_lines("input.txt")),
+        2
+    );
+}
