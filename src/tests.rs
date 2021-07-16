fe12f08f674d4eda69bde7a246608e7213697387
bOli <github@bueechi.net>
2021-07-16 20:53:39 +0200

    Solved part 2

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,4 @@
-use crate::cumulate_frequency_adjustments;
+use crate::{cumulate_frequency_adjustments, find_first_repeated_frequency};
 use line_reader::{read_file_to_lines, read_str_to_lines};
 
 const EXAMPLE_1: &str = "1
@@ -19,5 +19,21 @@ fn part_1() {
     assert_eq!(
         cumulate_frequency_adjustments(&read_file_to_lines("input.txt")),
         454
+    );
+}
+
+#[test]
+fn part_2_example_1() {
+    assert_eq!(
+        find_first_repeated_frequency(&read_str_to_lines(EXAMPLE_1)),
+        2
+    );
+}
+
+#[test]
+fn part_2() {
+    assert_eq!(
+        find_first_repeated_frequency(&read_file_to_lines("input.txt")),
+        566
     );
 }
