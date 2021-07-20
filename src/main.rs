29d80f28b0f9d7641ede6df3520aba95f87df145
bOli <github@bueechi.net>
2021-07-20 19:09:09 +0200

    Solved day 10, both parts

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
 use crate::day08::{input_metadata_sum, input_value};
 use crate::day09::high_score;
+use crate::day10::message;
 use line_reader::read_file_to_lines;
 
 mod day08;
 mod day09;
+mod day10;
 
 fn main() {
-    day08_part1();
-    day08_part2();
-    day09_part1();
-    day09_part2();
+    day08();
+    day09();
+    day10();
 }
 
-fn day08_part1() {
+fn day08() {
     assert_eq!(
         42146,
         input_metadata_sum(&read_file_to_lines("input/day08.txt")[0])
     );
-}
-
-fn day08_part2() {
     assert_eq!(
         26753,
         input_value(&read_file_to_lines("input/day08.txt")[0])
     );
 }
 
-fn day09_part1() {
+fn day09() {
     assert_eq!(374690, high_score(477, 70851));
+    assert_eq!(3_009_951_158, high_score(477, 7_085_100));
 }
 
-fn day09_part2() {
-    assert_eq!(3_009_951_158, high_score(477, 7_085_100));
+fn day10() {
+    let input = &read_file_to_lines("input/day10.txt");
+    assert_eq!(10511, message(input).1);
 }
