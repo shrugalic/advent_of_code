cecfdeda4650ae8a19f0551eac1c7d53b1c74d5d
bOli <github@bueechi.net>
2021-07-20 17:05:14 +0200

    Solved day 9, part 2.
    By using sort of a doubly linked list, insertion and removal became fast enough for the 100 times larger last marble value of part 2

--- src/day09.rs
+++ src/day09.rs
@@ -1,20 +1,51 @@
 type PlayerCount = usize;
 type Score = usize;
+type Marble = usize;
+
+struct Neighbors {
+    prev: Marble,
+    next: Marble,
+}
+impl Neighbors {
+    fn new(prev: Marble, next: Marble) -> Self {
+        Neighbors { prev, next }
+    }
+}
 
 pub(crate) fn high_score(players: PlayerCount, last_marble: Score) -> Score {
-    let mut marbles = vec![0];
+    let mut current: Marble = 0;
+    // linked_marbles works sort of like a doubly linked list, to keep track of the neighboring
+    // marbles in both directions (counter-clockwise and clockwise). Its index corresponds to the
+    // marble value, and the value contains indices of the previous and next marbles respectively.
+    let mut linked_marbles: Vec<Neighbors> = vec![Neighbors::new(current, current)];
     let mut scores = vec![0; players];
-    let mut curr = 0;
-    for marble in 1..=last_marble {
-        // println!("marble {}, curr = {}", marble, curr);
-        if marble % 23 == 0 {
-            let player = marble % players;
-            scores[player] += marble;
-            curr = (curr + marbles.len() - 7) % marbles.len();
-            scores[player] += marbles.remove(curr);
+    for placed in 1..=last_marble {
+        if placed % 23 == 0 {
+            // Set current to be clockwise of the marble to remove
+            for _ in 1..7 {
+                current = linked_marbles[current].prev;
+            }
+            // It will not actually be removed, just unlinked and never be linked to again
+            let removed: Marble = linked_marbles[current].prev;
+            let previous: Marble = linked_marbles[removed].prev;
+            linked_marbles[previous].next = current;
+            linked_marbles[current].prev = previous;
+
+            let player = placed % players;
+            scores[player] += removed;
+            scores[player] += placed;
+
+            // Insert dummy value to keep index-equals-marble-value property
+            linked_marbles.push(Neighbors::new(placed, placed));
         } else {
-            curr = (curr + 2) % marbles.len();
-            marbles.insert(curr, marble);
+            let next: Marble = linked_marbles[current].next;
+            let after_next: Marble = linked_marbles[next].next;
+            // The new marble goes in-between these two
+
+            current = placed;
+            linked_marbles[next].next = current;
+            linked_marbles[after_next].prev = current;
+            linked_marbles.push(Neighbors::new(next, after_next));
         }
     }
     *scores.iter().max().unwrap()
@@ -41,5 +72,10 @@ mod tests {
     #[test]
     fn part_1() {
         assert_eq!(374_690, high_score(477, 70_851));
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(3_009_951_158, high_score(477, 7_085_100));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,29 @@ mod day09;
 
 fn main() {
     day08_part1();
+    day08_part2();
+    day09_part1();
+    day09_part2();
 }
 
-pub fn day08_part1() {
+fn day08_part1() {
     assert_eq!(
         42146,
         input_metadata_sum(&read_file_to_lines("input/day08.txt")[0])
     );
 }
 
-pub fn day08_part2() {
+fn day08_part2() {
     assert_eq!(
         26753,
         input_value(&read_file_to_lines("input/day08.txt")[0])
     );
 }
 
-pub fn day09_part1() {
+fn day09_part1() {
     assert_eq!(374690, high_score(477, 70851));
+}
+
+fn day09_part2() {
+    assert_eq!(3_009_951_158, high_score(477, 7_085_100));
 }
