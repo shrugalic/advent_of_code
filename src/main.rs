36f4e537eb15c2118b4e1c7bffba18d9f3bd8951
bOli <github@bueechi.net>
2021-07-20 13:55:55 +0200

    Solved part 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::day08::input_metadata_sum;
+use crate::day08::{input_metadata_sum, input_value};
 use line_reader::read_file_to_lines;
 
 mod day08;
@@ -7,9 +7,16 @@ fn main() {
     day08_part1();
 }
 
-fn day08_part1() {
+pub fn day08_part1() {
     assert_eq!(
         42146,
         input_metadata_sum(&read_file_to_lines("input/day08.txt")[0])
+    );
+}
+
+pub fn day08_part2() {
+    assert_eq!(
+        26753,
+        input_value(&read_file_to_lines("input/day08.txt")[0])
     );
 }
