e1133aba81b1a5173d57f3e09de41124581c0553
bOli <github@bueechi.net>
2021-07-25 12:27:48 +0200

    Solved day 12 part 1

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@ use crate::day08::{input_metadata_sum, input_value};
 use crate::day09::high_score;
 use crate::day10::message;
 use crate::day11::{largest_total_power_3x3_square, largest_total_power_variable_size_square};
+use crate::day12::number_of_plants_after_20_gens;
 use line_reader::read_file_to_lines;
 
 mod day08;
 mod day09;
 mod day10;
 mod day11;
+mod day12;
 
 fn main() {
     day08();
     day09();
     day10();
     day11();
+    day12();
 }
 
 fn day08() {
@@ -42,5 +45,12 @@ fn day11() {
     assert_eq!(
         (119, 234, 272, 18),
         largest_total_power_variable_size_square(8199)
+    );
+}
+
+fn day12() {
+    assert_eq!(
+        325,
+        number_of_plants_after_20_gens(&read_file_to_lines("input/day12.txt"))
     );
 }
