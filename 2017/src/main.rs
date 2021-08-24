8e0cd94f5a5e4b8c3c40c62dc28a6b94f0ed670e
bOli <github@bueechi.net>
2021-08-24 20:55:41 +0200

    Solved 2017 day 4

--- 2017/src/main.rs
+++ 2017/src/main.rs
@@ -1,15 +1,18 @@
 use crate::day01::{day1_part1, day1_part2};
 use crate::day02::{day2_part1, day2_part2};
 use crate::day03::{day3_part1, day3_part2};
+use crate::day04::{day4_part1, day4_part2};
 
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {
     day01();
     day02();
     day03();
+    day04();
 }
 
 fn day01() {
@@ -25,4 +28,9 @@ fn day02() {
 fn day03() {
     assert_eq!(326, day3_part1());
     assert_eq!(363010, day3_part2());
+}
+
+fn day04() {
+    assert_eq!(451, day4_part1());
+    assert_eq!(223, day4_part2());
 }
