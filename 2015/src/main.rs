40f4789b07b102792a954c58f899092763b104b6
bOli <github@bueechi.net>
2021-09-21 17:07:03 +0200

    Use consistent leading zero for day numbers
    For less-error prone renaming of new days ;)

--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -1,16 +1,15 @@
-use crate::day01::{day1_part1, day1_part2};
-use crate::day02::{day2_part1, day2_part2};
-
 mod day01;
+use crate::day01::{day01_part1, day01_part2};
 fn day01() {
-    assert_eq!(280, day1_part1());
-    assert_eq!(1797, day1_part2());
+    assert_eq!(280, day01_part1());
+    assert_eq!(1797, day01_part2());
 }
 
 mod day02;
+use crate::day02::{day02_part1, day02_part2};
 fn day02() {
-    assert_eq!(1588178, day2_part1());
-    assert_eq!(3783758, day2_part2());
+    assert_eq!(1588178, day02_part1());
+    assert_eq!(3783758, day02_part2());
 }
 
 fn main() {
