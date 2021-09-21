d0eb9fd9bc840e681b7a11bd98556bca2ad2b382
bOli <github@bueechi.net>
2021-09-21 16:55:54 +0200

    Solved 2015 day 2

--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -1,4 +1,5 @@
 use crate::day01::{day1_part1, day1_part2};
+use crate::day02::{day2_part1, day2_part2};
 
 mod day01;
 fn day01() {
@@ -6,6 +7,13 @@ fn day01() {
     assert_eq!(1797, day1_part2());
 }
 
+mod day02;
+fn day02() {
+    assert_eq!(1588178, day2_part1());
+    assert_eq!(3783758, day2_part2());
+}
+
 fn main() {
     day01();
+    day02();
 }
