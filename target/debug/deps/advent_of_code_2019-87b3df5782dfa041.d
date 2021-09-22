1efd955c995ad148bd7aaa9bd8815a31b542b299
bOli <github@bueechi.net>
2021-09-22 15:22:42 +0200

    2019: Removed unnecessary target dir

--- target/debug/deps/advent_of_code_2019-87b3df5782dfa041.d
+++
@@ -1,8 +0,0 @@
-/Users/oli/Code/advent_of_code/2019/target/debug/deps/advent_of_code_2019-87b3df5782dfa041.rmeta: src/main.rs src/day01.rs
-
-/Users/oli/Code/advent_of_code/2019/target/debug/deps/advent_of_code_2019-87b3df5782dfa041.d: src/main.rs src/day01.rs
-
-src/main.rs:
-src/day01.rs:
-
-# env-dep:CLIPPY_ARGS=
