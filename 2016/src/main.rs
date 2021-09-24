dd1f8e500c468d587e44c086817182268463844c
bOli <github@bueechi.net>
2021-09-24 23:34:51 +0200

    Solved 2016 day 2

--- 2016/src/main.rs
+++ 2016/src/main.rs
@@ -5,6 +5,14 @@ fn day01() {
     assert_eq!(1797, day01_part2());
 }
 
+mod day02;
+use crate::day02::{day02_part1, day02_part2};
+fn day02() {
+    assert_eq!("99332", day02_part1());
+    assert_eq!("DD483", day02_part2());
+}
+
 fn main() {
     day01();
+    day02();
 }
