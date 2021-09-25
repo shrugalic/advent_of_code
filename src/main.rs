b3bd41905d6fb9312d75ee04a4af286ee461aa88
bOli <github@bueechi.net>
2021-09-25 13:04:31 +0200

    Fixed 2016 day 1 assertions in main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod day01;
 use crate::day01::{day01_part1, day01_part2};
 fn day01() {
-    assert_eq!(280, day01_part1());
-    assert_eq!(1797, day01_part2());
+    assert_eq!(230, day01_part1());
+    assert_eq!(154, day01_part2());
 }
 
 mod day02;
