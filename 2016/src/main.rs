8eda7e6e519341556d824ff803b6245fce045915
bOli <github@bueechi.net>
2021-09-25 13:28:46 +0200

    Solved 2016 day 3

--- 2016/src/main.rs
+++ 2016/src/main.rs
@@ -12,7 +12,15 @@ fn day02() {
     assert_eq!("DD483", day02_part2());
 }
 
+mod day03;
+use crate::day03::{day03_part1, day03_part2};
+fn day03() {
+    assert_eq!(1050, day03_part1());
+    assert_eq!(1921, day03_part2());
+}
+
 fn main() {
     day01();
     day02();
+    day03();
 }
