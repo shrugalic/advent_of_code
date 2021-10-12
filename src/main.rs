9bf01a37e27280df9de27f1caae33c3cf2a2ac65
bOli <github@bueechi.net>
2021-10-12 17:52:54 +0200

    Solved 2016 day 14

--- src/main.rs
+++ src/main.rs
@@ -89,6 +89,13 @@ fn day13() {
     assert_eq!(124, day13_part2());
 }
 
+mod day14;
+use crate::day14::{day14_part1, day14_part2};
+fn day14() {
+    assert_eq!(23_890, day14_part1());
+    assert_eq!(22_696, day14_part2());
+}
+
 fn main() {
     day01();
     day02();
@@ -103,4 +110,5 @@ fn main() {
     day11();
     day12();
     day13();
+    day14();
 }
