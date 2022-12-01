1943fc21b085cf5f62b91b0ec80d2e16f5b19bc4
bOli <github@bueechi.net>
2022-12-01 06:47:32 +0100

    Solved 2022 day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,29 +1,37 @@
+use std::cmp::Reverse;
+
 const INPUT: &str = include_str!("../input/day01.txt");
 
 pub(crate) fn day01_part1() -> usize {
-    let depths = parse(INPUT);
-    part1(&depths)
+    let supplies = parse_supplies(INPUT);
+    largest_total_supply(&supplies)
 }
 
 pub(crate) fn day01_part2() -> usize {
-    let depths = parse(INPUT);
-    part2(&depths)
+    let supplies = parse_supplies(INPUT);
+    sum_of_largest_three_total_supplies(&supplies)
 }
 
-fn parse(input: &str) -> Vec<usize> {
+fn parse_supplies(input: &str) -> Vec<Vec<usize>> {
     input
         .trim()
-        .lines()
-        .filter_map(|s| s.parse().ok())
+        .split("\n\n")
+        .filter_map(|s| s.lines().map(|s| s.parse().ok()).collect())
         .collect()
 }
 
-fn part1(depths: &[usize]) -> usize {
-    depths.windows(2).filter(|w| w[0] < w[1]).count()
+fn calculate_totals(supplies: &[Vec<usize>]) -> Vec<usize> {
+    supplies.iter().map(|v| v.iter().sum()).collect()
 }
 
-fn part2(depths: &[usize]) -> usize {
-    depths.windows(4).filter(|w| w[0] < w[3]).count()
+fn largest_total_supply(supplies: &[Vec<usize>]) -> usize {
+    *calculate_totals(supplies).iter().max().unwrap()
+}
+
+fn sum_of_largest_three_total_supplies(supplies: &[Vec<usize>]) -> usize {
+    let mut totals = calculate_totals(supplies);
+    totals.sort_unstable_by_key(|&k| Reverse(k));
+    totals.iter().take(3).sum()
 }
 
 #[cfg(test)]
@@ -31,36 +39,70 @@ mod tests {
     use super::*;
 
     const EXAMPLE: &str = "\
-199
-200
-208
-210
-200
-207
-240
-269
-260
-263";
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
 
     #[test]
-    fn test_count_increasing_depths() {
-        let depths = parse(EXAMPLE);
-        assert_eq!(7, part1(&depths));
+    fn test_parse_supplies() {
+        let supplies = parse_supplies(EXAMPLE);
+        assert_eq!(
+            vec![
+                vec![1_000, 2_000, 3_000],
+                vec![4_000],
+                vec![5_000, 6_000],
+                vec![7_000, 8_000, 9_000],
+                vec![10_000],
+            ],
+            supplies
+        );
     }
 
     #[test]
-    fn test_count_increasing_3_depth_averages() {
-        let depths = parse(EXAMPLE);
-        assert_eq!(5, part2(&depths));
+    fn test_calculate_totals() {
+        let supplies = vec![
+            vec![1_000, 2_000, 3_000],
+            vec![4_000],
+            vec![5_000, 6_000],
+            vec![7_000, 8_000, 9_000],
+            vec![10_000],
+        ];
+        assert_eq!(
+            vec![6_000, 4_000, 11_000, 24_000, 10_000],
+            calculate_totals(&supplies)
+        );
+    }
+
+    #[test]
+    fn test_largest_total_supply() {
+        let supplies = parse_supplies(EXAMPLE);
+        assert_eq!(24_000, largest_total_supply(&supplies));
+    }
+
+    #[test]
+    fn test_sum_of_largest_three_total_supplies() {
+        let supplies = parse_supplies(EXAMPLE);
+        assert_eq!(45_000, sum_of_largest_three_total_supplies(&supplies));
     }
 
     #[test]
     fn test_part1() {
-        assert_eq!(day01_part1(), 1475);
+        assert_eq!(day01_part1(), 72_240);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(day01_part2(), 1516);
+        assert_eq!(day01_part2(), 210_957);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use crate::day01::{day01_part1, day01_part2};
 mod day01;
 fn day01() {
-    assert_eq!(1475, day01_part1());
-    assert_eq!(1516, day01_part2());
+    assert_eq!(72_240, day01_part1());
+    assert_eq!(210_957, day01_part2());
 }
 
 fn main() {
