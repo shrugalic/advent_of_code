c37a1a30cbe6c584778dad21b8d738d3d63581c2
bOli <github@bueechi.net>
2022-12-07 22:11:05 +0100

    Slightly more structured approach

--- src/day07.rs
+++ src/day07.rs
@@ -2,56 +2,70 @@ use std::collections::HashMap;
 
 const INPUT: &str = include_str!("../input/day07.txt");
 
-/// A path to directory or file
-type Path = Vec<String>;
-
-/// A directory or file name and size
-type NameAndSize = (&'static str, usize);
-
 pub(crate) fn day07_part1() -> usize {
-    let lines = parse(INPUT);
-    let file_file_name_and_size_by_path = file_file_name_and_size_by_path(lines);
-    let dir_size_by_path = sum_of_directory_sizes(file_file_name_and_size_by_path);
+    let file_list = generate_file_list(INPUT);
+    let dir_size_by_path = sum_of_directory_sizes(file_list);
     sum_of_directories_smaller_than_100_000(dir_size_by_path)
 }
 
 pub(crate) fn day07_part2() -> usize {
-    let lines = parse(INPUT);
-    let file_name_and_size_by_path = file_file_name_and_size_by_path(lines);
+    let file_name_and_size_by_path = generate_file_list(INPUT);
     let dir_size_by_path = sum_of_directory_sizes(file_name_and_size_by_path);
     smallest_directory_large_enough(dir_size_by_path)
 }
 
-fn sum_of_directory_sizes(
-    file_name_and_size_by_path: HashMap<Path, Vec<NameAndSize>>,
-) -> HashMap<String, usize> {
+fn generate_file_list(input: &'static str) -> Vec<FileOrDir> {
+    let mut path = vec![];
+    let mut files_and_directories = Vec::new();
+    for line in input.trim().lines() {
+        let parts: Vec<_> = line.split_ascii_whitespace().collect();
+        match parts[0] {
+            "$" => match parts[1] {
+                "cd" => match parts[2] {
+                    "/" => path.push("/".to_string()),
+                    ".." => {
+                        path.pop();
+                    }
+                    dir => path.push(format!("{dir}/")),
+                },
+                "ls" => {}
+                cmd => unreachable!("unexpected command '{}'", cmd),
+            },
+            "dir" => {
+                files_and_directories.push(FileOrDir::directory(&path.join(""), parts[1]));
+            }
+            file_size => {
+                let file_size = file_size.parse().expect("a file size");
+                files_and_directories.push(FileOrDir::file(&path.join(""), parts[1], file_size))
+            }
+        }
+    }
+    files_and_directories
+}
+
+fn sum_of_directory_sizes(file_list: Vec<FileOrDir>) -> HashMap<String, usize> {
     // max-depth to start directory size accumulation
-    let mut depth: usize = file_name_and_size_by_path
-        .keys()
-        .map(|path| path.len())
+    let mut depth: usize = file_list
+        .iter()
+        .map(FileOrDir::depth)
         .max()
         .expect("at least one directory");
     let mut dir_size_by_path: HashMap<String, usize> = HashMap::new();
     while depth > 0 {
-        for (path, sizes_and_names) in file_name_and_size_by_path
-            .iter()
-            .filter(|(path, _)| path.len() == depth)
-        {
-            let dir = path.join("");
-            let mut total_size = 0;
-            for (name, file_size) in sizes_and_names {
-                if *file_size == 0 {
-                    // directory -> find its size
-                    let sub_dir = format!("{dir}{name}/");
-                    total_size += dir_size_by_path
-                        .get(&sub_dir)
+        for file_or_dir in file_list.iter().filter(|&file| file.depth() == depth) {
+            match file_or_dir {
+                FileOrDir::File { size, .. } => {
+                    *dir_size_by_path.entry(file_or_dir.parent()).or_default() += size
+                }
+                FileOrDir::Directory { .. } => {
+                    // Append trailing slash to directory path to look same as parent
+                    let path = format!("{}/", file_or_dir.path());
+                    let dir_size = *dir_size_by_path
+                        .get(&path)
                         .expect("directory to be present");
-                } else {
-                    // file -> has known size
-                    total_size += file_size;
+                    *dir_size_by_path.entry(file_or_dir.parent()).or_default() += dir_size;
                 }
             }
-            dir_size_by_path.insert(dir, total_size);
         }
         depth -= 1;
     }
@@ -66,54 +80,48 @@ fn sum_of_directories_smaller_than_100_000(dir_size_by_path: HashMap<String, usi
 }
 
 const DISK_SIZE: usize = 70_000_000;
-const TOTAL_NEEDED_DISK_SPACE: usize = 30_000_000;
-
+const NEEDED_SPACE: usize = 30_000_000;
 fn smallest_directory_large_enough(dir_size_by_path: HashMap<String, usize>) -> usize {
-    let total_size = dir_size_by_path.get("/").expect("a root directory");
-    let free_disk_space = DISK_SIZE - total_size;
-    let needed_disk_space = TOTAL_NEEDED_DISK_SPACE - free_disk_space;
+    let total_used = dir_size_by_path.get("/").expect("a root directory");
+    let free_space = DISK_SIZE - total_used;
+    let min_space_to_clear = NEEDED_SPACE - free_space;
     *dir_size_by_path
         .values()
-        .filter(|&&size| size >= needed_disk_space)
+        .filter(|&&dir_size| dir_size >= min_space_to_clear)
         .min()
         .expect("at least one entry to get min of")
 }
 
-fn file_file_name_and_size_by_path(lines: Vec<&str>) -> HashMap<Path, Vec<(&str, usize)>> {
-    let mut directory_stack = vec![];
-    let mut file_name_and_size_by_path: HashMap<Path, Vec<(&str, usize)>> = HashMap::new();
-    for line in lines {
-        let parts: Vec<_> = line.split_ascii_whitespace().collect();
-        match parts[0] {
-            "$" => match parts[1] {
-                "cd" => match parts[2] {
-                    "/" => directory_stack.push("/".to_string()),
-                    ".." => {
-                        directory_stack.pop();
-                    }
-                    dir => directory_stack.push(format!("{dir}/")),
-                },
-                "ls" => {}
-                cmd => unreachable!("unexpected command '{}'", cmd),
-            },
-            dir_or_file => {
-                let contents = file_name_and_size_by_path
-                    .entry(directory_stack.clone())
-                    .or_default();
-                let size = if dir_or_file == "dir" {
-                    0
-                } else {
-                    dir_or_file.parse().expect("a file size")
-                };
-                contents.push((parts[1], size));
-            }
+enum FileOrDir {
+    File { size: usize, path: String },
+    Directory { path: String },
+}
+impl FileOrDir {
+    fn directory(parent: &str, name: &str) -> Self {
+        let path = format!("{parent}{name}");
+        FileOrDir::Directory { path }
+    }
+    fn file(parent: &str, name: &str, size: usize) -> Self {
+        let path = format!("{parent}{name}");
+        FileOrDir::File { size, path }
+    }
+    fn depth(&self) -> usize {
+        self.path().chars().filter(|&c| c == '/').count()
+    }
+    fn path(&self) -> &str {
+        match self {
+            FileOrDir::File { path, .. } => path,
+            FileOrDir::Directory { path } => path,
         }
     }
-    file_name_and_size_by_path
-}
-
-fn parse(lines: &str) -> Vec<&str> {
-    lines.trim().lines().collect()
+    fn parent(&self) -> String {
+        let path = self.path();
+        if let Some(i) = path.rfind('/') {
+            path[0..=i].to_string()
+        } else {
+            unreachable!();
+        }
+    }
 }
 
 #[cfg(test)]
@@ -147,8 +155,7 @@ $ ls
 
     #[test]
     fn part1_example() {
-        let lines = parse(EXAMPLE);
-        let file_name_and_size_by_path = file_file_name_and_size_by_path(lines);
+        let file_name_and_size_by_path = generate_file_list(EXAMPLE);
         let dir_size_by_path = sum_of_directory_sizes(file_name_and_size_by_path);
         assert_eq!(
             94853 + 584, // = 95437
@@ -158,8 +165,7 @@ $ ls
 
     #[test]
     fn part2_example() {
-        let lines = parse(EXAMPLE);
-        let file_name_and_size_by_path = file_file_name_and_size_by_path(lines);
+        let file_name_and_size_by_path = generate_file_list(EXAMPLE);
         let dir_size_by_path = sum_of_directory_sizes(file_name_and_size_by_path);
         assert_eq!(
             24_933_642,
